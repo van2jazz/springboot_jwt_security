68bc1a0ad8a502e09caaa9c23fb5d4f9bfadd7a7
Dayo <van2jazz@gmail.com>
2023-07-25 11:01:12 +0100

    initial commit

---
+++ src/main/java/com/demo/demo/entity/Role.java
@@ -0,0 +1,5 @@
+package com.demo.demo.entity;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into _user(_user_id, firstname, lastname, email, password)
+values(1001, 'jade', 'bate', 'password@hhyh.bn', 'password');
+
